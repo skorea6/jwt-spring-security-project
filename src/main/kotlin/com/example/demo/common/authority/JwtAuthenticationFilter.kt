0e1d500cc3860535d8030eb1ddd198fcfce01f3f
skorea6 <skorea6@naver.com>
2023-12-23 14:18:20 +0900

    Jwt Auth Filter

---
+++ src/main/kotlin/com/example/demo/common/authority/JwtAuthenticationFilter.kt
@@ -0,0 +1,43 @@
+package com.example.demo.common.authority
+
+import com.example.demo.common.exception.ApiCustomException
+import com.example.demo.member.repository.MemberRepository
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.GenericFilterBean
+
+class JwtAuthenticationFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+) : GenericFilterBean() {
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        try {
+            val token = resolveToken(request as HttpServletRequest)
+
+            if (jwtTokenProvider.validateAccessTokenForFilter(token)) {
+                val authentication = jwtTokenProvider.getAuthentication(token)
+                SecurityContextHolder.getContext().authentication = authentication
+            }
+        } catch (e: Exception) {
+            request?.setAttribute("exception", e)
+        }
+
+        chain?.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest): String {
+        val bearerToken = request.getHeader("Authorization")
+
+        return if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            bearerToken.substring(7)
+        } else {
+            throw ApiCustomException(HttpStatus.UNAUTHORIZED.value(), "인증이 필요한 서비스입니다.")
+        }
+    }
+}
