4cd079920083e99fc2c098186cb76e3b7683c616
skorea6 <skorea6@naver.com>
2023-12-26 23:54:52 +0900

    RefreshTokenInfoDto : Redis value에 저장할 DTO

---
+++ src/main/kotlin/com/example/demo/common/redis/dto/RefreshTokenInfoDto.kt
@@ -0,0 +1,49 @@
+package com.example.demo.common.redis.dto
+
+import com.example.demo.util.DateUtil
+import com.example.demo.util.RandomUtil
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotBlank
+import java.io.Serializable
+
+data class RefreshTokenInfoDto(
+    val userId: String = "",
+    val refreshToken: String = "",
+    val header: String = "",
+    val browser: String = "",
+    val os: String = "",
+    val ipAddress: String = "",
+    val secret: String = RandomUtil().generateRandomString(15),
+    val date: String = DateUtil().getCurrentTime(),
+) : Serializable {
+    fun toResponse(): RefreshTokenInfoDtoResponse {
+        return RefreshTokenInfoDtoResponse(
+            userId = userId,
+            header = header,
+            browser = browser,
+            os = os,
+            ipAddress = ipAddress,
+            secret = secret,
+            date = date
+        )
+    }
+}
+
+data class RefreshTokenInfoDtoResponse(
+    val userId: String,
+    val header: String?,
+    val browser: String?,
+    val os: String?,
+    val ipAddress: String?,
+    val secret: String,
+    val date: String
+) : Serializable
+
+data class RefreshTokenDeleteDto(
+    @field:NotBlank
+    @JsonProperty("secret")
+    private val _secret: String?,
+) {
+    val secret: String
+        get() = _secret!!
+}
