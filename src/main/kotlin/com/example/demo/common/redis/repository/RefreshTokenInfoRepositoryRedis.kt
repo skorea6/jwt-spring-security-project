fe3a38526fd92a93c2ae95b0219f302bc59b9c08
skorea6 <skorea6@naver.com>
2023-12-28 13:54:02 +0900

    RefreshTokenInfoRepositoryRedis : refreshTokenDto를 value로 갖게끔 수정

--- src/main/kotlin/com/example/demo/common/redis/repository/RefreshTokenInfoRepositoryRedis.kt
+++ src/main/kotlin/com/example/demo/common/redis/repository/RefreshTokenInfoRepositoryRedis.kt
@@ -1,7 +1,10 @@
 package com.example.demo.common.redis.repository
 
 import com.example.demo.common.authority.REFRESH_EXPIRATION_MILLISECONDS
+import com.example.demo.common.redis.dto.RefreshTokenInfoDto
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.stereotype.Repository
 import java.util.concurrent.TimeUnit
 
@@ -12,20 +15,33 @@ import java.util.concurrent.TimeUnit
 
 @Repository
 class RefreshTokenInfoRepositoryRedis(
-    private val redisTemplate: RedisTemplate<String, String>
+    private val redisTemplate: RedisTemplate<String, RefreshTokenInfoDto>
 ) {
     companion object {
         private const val KEY_PREFIX = "refreshToken" // refreshToken:{userId}:{refreshToken}
     }
 
-    fun save(userId: String, refreshToken: String) {
-        val key = "$KEY_PREFIX:$userId:$refreshToken"
-        redisTemplate.opsForValue().set(key, "", REFRESH_EXPIRATION_MILLISECONDS, TimeUnit.MILLISECONDS)
+    init {
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
     }
 
-    fun findByRefreshToken(refreshToken: String): String? {
+    fun save(refreshTokenInfoDto: RefreshTokenInfoDto) {
+        val key = "$KEY_PREFIX:${refreshTokenInfoDto.userId}:${refreshTokenInfoDto.refreshToken}"
+        redisTemplate.opsForValue().set(key, refreshTokenInfoDto, REFRESH_EXPIRATION_MILLISECONDS, TimeUnit.MILLISECONDS)
+    }
+
+    fun findByRefreshToken(refreshToken: String): RefreshTokenInfoDto? {
         val key = redisTemplate.keys("$KEY_PREFIX:*:$refreshToken").firstOrNull()
         return key?.let { redisTemplate.opsForValue().get(it) }
+    }
+
+    fun findByUserId(userId: String): List<RefreshTokenInfoDto> {
+        val keys = redisTemplate.keys("$KEY_PREFIX:$userId:*")
+        val limitedKeys = keys.take(100)
+        return limitedKeys.mapNotNull { key ->
+            key?.let { redisTemplate.opsForValue().get(it) }
+        }
     }
 
     fun deleteByRefreshToken(refreshToken: String) {
@@ -36,5 +52,16 @@ class RefreshTokenInfoRepositoryRedis(
     fun deleteByUserId(userId: String) {
         val keys = redisTemplate.keys("$KEY_PREFIX:$userId:*")
         redisTemplate.delete(keys)
+    }
+
+    fun deleteBySecret(userId: String, secret: String): Boolean {
+        val keys = redisTemplate.keys("$KEY_PREFIX:$userId:*")
+        val refreshTokenInfoDto: RefreshTokenInfoDto = keys.mapNotNull { key ->
+            key?.let { redisTemplate.opsForValue().get(it) }
+        }.firstOrNull { x -> x.secret == secret } ?: return false
+
+        val key = redisTemplate.keys("$KEY_PREFIX:${refreshTokenInfoDto.userId}:${refreshTokenInfoDto.refreshToken}")
+        redisTemplate.delete(key)
+        return true
     }
 }
