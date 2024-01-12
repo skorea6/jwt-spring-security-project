5668dd2d8e942d177c372064986eb18ba3f17d1e
skorea6 <skorea6@naver.com>
2024-01-12 11:27:25 +0900

    build.gradle.kts - AWS 이메일 관련 dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("com.amazonaws:aws-java-sdk-ses:1.12.6")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
