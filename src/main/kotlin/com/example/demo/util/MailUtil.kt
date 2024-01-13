35612ff3dd1bd0bf48c297e5d2637659860c9387
skorea6 <skorea6@naver.com>
2024-01-13 13:04:14 +0900

    MailUtil - SenderDto 추가 및 이메일 발송 로직

---
+++ src/main/kotlin/com/example/demo/util/MailUtil.kt
@@ -0,0 +1,57 @@
+package com.example.demo.util
+
+import com.amazonaws.AmazonClientException
+import com.amazonaws.services.simpleemail.model.*
+import com.example.demo.common.config.aws.AwsSesConfig
+import org.springframework.stereotype.Component
+
+
+data class SenderDto(
+    val from: String = "ABZ 관리자 <no-reply@abz.kr>",
+    val to: ArrayList<String>,
+    val subject: String,
+    val content: String
+) {
+    fun addTo(email: String) {
+        to.add(email)
+    }
+
+    fun toSendRequestDto(): SendEmailRequest {
+        val destination: Destination = Destination()
+            .withToAddresses(to)
+        val message: Message = Message()
+            .withSubject(createContent(subject))
+            .withBody(
+                Body()
+                    .withHtml(createContent(content))
+            )
+        return SendEmailRequest()
+            .withSource(from)
+            .withDestination(destination)
+            .withMessage(message)
+    }
+
+    private fun createContent(text: String): Content {
+        return Content()
+            .withCharset("UTF-8")
+            .withData(text)
+    }
+}
+
+
+@Component
+class MailUtil(
+    private val awsSesConfig: AwsSesConfig
+) {
+    fun send(senderDto: SenderDto) {
+        try {
+            val client = awsSesConfig.amazonSimpleEmailService()
+            client.sendEmail(senderDto.toSendRequestDto())
+        } catch (ex: Exception) {
+            throw AmazonClientException(
+                ex.message,
+                ex
+            )
+        }
+    }
+}
