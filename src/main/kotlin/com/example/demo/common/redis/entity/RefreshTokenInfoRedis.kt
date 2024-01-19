780e6495ac2b101e01a3184cd9127905520566a1
skorea6 <skorea6@naver.com>
2024-01-19 19:56:19 +0900

    RefreshTokenInfoRedis - 사용 안하므로 주석 처리

--- src/main/kotlin/com/example/demo/common/redis/entity/RefreshTokenInfoRedis.kt
+++ src/main/kotlin/com/example/demo/common/redis/entity/RefreshTokenInfoRedis.kt
@@ -1,16 +1,14 @@
 package com.example.demo.common.redis.entity
 
-import com.example.demo.common.login.jwt.REFRESH_EXPIRATION_MILLISECONDS
-import org.springframework.data.annotation.Id
-import org.springframework.data.redis.core.RedisHash
-
 /**
  * 아래 @RedisHash 방법은 사용하지 않고, 순수 RedisTemplate을 사용하기로 결정함
  * 조금 더 커스텀하게 가져가고 불필요한 set들이 만들어지는 것을 방지하기 위함.
  */
+/**
 @RedisHash(value = "refreshTokenInfo", timeToLive = REFRESH_EXPIRATION_MILLISECONDS /1000)
 data class RefreshTokenInfoRedis (
     @Id
     val refreshToken : String,
     val userId : String,
 )
+*/
