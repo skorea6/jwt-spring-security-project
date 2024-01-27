88f3f2bd8b73366fc67e08ebe48c97d88e3777d3
skorea6 <skorea6@naver.com>
2024-01-27 21:19:47 +0900

    timezone을 Asia/Seoul로 설정

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,12 +1,19 @@
 package com.example.demo
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import java.util.*
 
 @EnableCaching
 @SpringBootApplication
-class DemoApplication
+class DemoApplication{
+	@PostConstruct
+	fun started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
